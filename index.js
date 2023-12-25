e9eedfaf393042c85bfac54ac781ac5a38633a90
Tatiana Mikhailova <imagetata@gmail.com>
2023-12-25 12:35:21 +0100

    added function with date

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const prompt = require('prompt-sync')();
 
 function greet(namePassedIn) {
-  return 'Welcome to SALT, ';
+  return 'Welcome to SALT, ' + namePassedIn;
+}
+
+function days(datePassedIn) {
+  const startDate = new Date(datePassedIn);
+  const today = new Date();
+  const oneDay = 24 * 60 * 60 * 1000;
+  return Math.round(Math.abs((startDate - today) / oneDay));
 }
 
 // Main program
 const name = prompt('What is your name? ');
+const startDay = prompt('When does that course start (yyyy-mm-ddaaaa)? ');
 const greeting = greet(name);
+const daysCount = days(startDay);
 console.log(greeting);
+console.log('Today it is ' + daysCount + ' days left until the course starts');
 
 module.exports.greet = greet;
+module.exports.days = days;
